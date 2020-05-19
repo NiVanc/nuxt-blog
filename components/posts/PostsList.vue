ff049e7673522089303e06e23a326e048718a5a3
NiVanc <nico@izebra.be>
2020-05-20 01:09:48 +0200

    Add dynamic data on the homepage for the posts

--- components/posts/PostsList.vue
+++ components/posts/PostsList.vue
@@ -1,25 +1,13 @@
 <template>
   <section class="posts-list">
     <PostPreview
-      id="1"
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
       :is-admin="isAdmin"
-      thumbnail="https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg"
-      title="Hello there!"
-      previewText="Don't hesitate. Click to read more."
-    />
-    <PostPreview
-      id="2"
-      :is-admin="isAdmin"
-      thumbnail="https://www.angelpunk.co/wp-content/uploads/MzI4MDQ2Mg.jpeg"
-      title="More exciting news!"
-      previewText="I know you're tempted. Just click to read more."
-    />
-    <PostPreview
-      id="3"
-      :is-admin="isAdmin"
-      thumbnail="https://lh3.googleusercontent.com/proxy/_nhMHrzRvqRsZ_ku43iueQ9fxeT18gSIYVQQSsyO7CzYsDLB41SPdvZ7eEGAtO5E9Pq9HyJjJnbf0LscYxXZxQAYJv4Bib_74tIpCz7Ub1M9LfpW4HmE7pGwIKu4SPsDwqzTAW4I5YR4LElsPwpxs5Ijow"
-      title="Clickbait title"
-      previewText="You must click this one for sure."
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText"
     />
   </section>
 </template>
@@ -35,6 +23,10 @@ export default {
     isAdmin: {
       type: Boolean,
       default: false
+    },
+    posts: {
+      type: Array,
+      required: true
     }
   }
 };
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="intro">
       <h1>Get the latest tech news.</h1>
     </section>
-    <PostsList />
+    <PostsList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -13,6 +13,26 @@ import PostsList from "~/components/posts/PostsList";
 export default {
   components: {
     PostsList
+  },
+  data() {
+    return {
+      loadedPosts: [
+        {
+          id: "1",
+          title: "First Post",
+          previewText: "This is our first post",
+          thumbnail:
+            "https://www.angelpunk.co/wp-content/uploads/MzI4MDQ2Mg.jpeg"
+        },
+        {
+          id: "2",
+          title: "Second Post",
+          previewText: "This is our second post",
+          thumbnail:
+            "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg"
+        }
+      ]
+    };
   }
 };
 </script>
