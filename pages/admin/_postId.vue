3267cef2355528695555c0db1c9e5cdf24a23466
NiVanc <nico@izebra.be>
2020-05-20 00:47:45 +0200

    Setup the admin pages

---
+++ pages/admin/_postId.vue
@@ -0,0 +1,40 @@
+<template>
+  <div admin-post-page>
+    <section class="update-form">
+      <AdminPostForm :post="LoadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "~/components/Admin/AdminPostForm";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      LoadedPost: {
+        author: "Nico",
+        title: "My awesome post",
+        content: "Super amazing content here.",
+        thumbnailLink:
+          "https://www.angelpunk.co/wp-content/uploads/MzI4MDQ2Mg.jpeg"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
