46b4860182ecbce35db33e68c2321338518f3ae5
NiVanc <nico@izebra.be>
2020-05-20 01:36:06 +0200

    Prepare the data server-side for the blog page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostsList />
+    <PostsList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -10,6 +10,28 @@ import PostsList from "~/components/posts/PostsList";
 export default {
   components: {
     PostsList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is our first post",
+            thumbnail:
+              "https://www.angelpunk.co/wp-content/uploads/MzI4MDQ2Mg.jpeg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is our second post",
+            thumbnail:
+              "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg"
+          }
+        ]
+      });
+    }, 1500);
   }
 };
 </script>
